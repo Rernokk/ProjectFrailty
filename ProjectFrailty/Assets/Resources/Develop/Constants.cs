d65921ee977374ade91730a2dc6aaaa947bb3aa6
Shannon Smith <smithsr1996@gmail.com>
2020-01-21 21:24:47 -0700

    Bitmask implementation

---
+++ ProjectFrailty/Assets/Resources/Develop/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants 
+{
+	public class ResourceDirectories
+	{
+		public const string MaskPairCSV = "Develop/MaskPairs";
+	}
+}
---
+++ ProjectFrailty/Assets/_Project/Scripts/Debug/BitmaskDisplay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BitmaskDisplay : MonoBehaviour
+{
+	[SerializeField]
+	private int bitValue = 0, spriteIndex = 0;
+	
+	public int BitValue
+	{
+		get
+		{
+			return bitValue;
+		}
+
+		set
+		{
+			bitValue = value;
+			transform.name = bitValue.ToString();
+			spriteIndex = GenerateBaseMap.MaskValuePairs[BitValue];
+		}
+	}
+
+	public int SpriteIndex
+	{
+		get
+		{
+			return spriteIndex;
+		}
+	}
+}
